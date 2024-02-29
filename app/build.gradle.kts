28a222f49370100e6dcafe95dee3a16cd22f2799
AllenTom <qweasdzxczse2@gmail.com>
2024-02-29 17:39:56 +0800

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.allentom.diffusion"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "0.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
